5097b54067e61925ffc41c5ddd2191c3603769bb
Guoshian <Guoshian>
2017-01-19 22:39:45 +0800

    add check block in pro1

--- app/src/main/java/eu/faircode/netguard/ActivityPro1.java
+++ app/src/main/java/eu/faircode/netguard/ActivityPro1.java
@@ -2,12 +2,19 @@ package eu.faircode.netguard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class ActivityPro1 extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_pro1);
+
+
+
     }
 }
